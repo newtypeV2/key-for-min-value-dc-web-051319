de819f8307a93a2c46b0eb5a9067b9c2a54067ea
Gian Libunao <gianlibunao@gmail.com>
2019-05-05 20:50:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
 lowest=nil
 if name_hash.length!=0
 lowestvalue = name_hash.first[1]
-lowest=name.hash.first[0]
+lowest=name_hash.first[0]
 name_hash.each { |x,y|
 if y>lowestvalue
   lowestvalue=y
